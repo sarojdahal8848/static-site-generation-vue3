7e797b3788f3cf1754b5edfa5a5b27bb296d0235
saroj dahal <sarojdahal8848@gmail.com>
2024-08-16 22:36:10 +0545

    meta tag

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,84 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue';
+import { useHead } from '@unhead/vue'
+
+const product = ref({})
+
+onMounted(async () => {
+  const res = await fetchData();
+  product.value = res;
+})
+
+async function fetchData() {
+  const res = await fetch('https://dummyjson.com/products/1')
+  return res.json()
+}
+
+
+
+const metaData = ref({
+      title: 'NRN Legal by MeroAdda®',
+      description: `NRN Legal is a prominent legal service of
+      MeroAdda® catering to the needs of Nepali citizens living
+      abroad. It aims to support and provide legal assistance to
+      Nepali expatriates around the world. NRN Legal plays an
+      important role in safeguarding the rights and interests
+      of Nepali nationals residing outside their home country.
+      NRN Legal, provided by MeroAdda®, an online service provider,
+      collaborates with legal professionals and experts to ensure
+      that Nepali expatriates receive comprehensive legal solutions.`,
+      url: `https://www.google.com`,
+    })
+
+useHead({
+      title: metaData.value.title,
+      meta: [
+        {
+          name: 'description',
+          content: () => metaData.value.description,
+        },
+        {
+          property: 'og:url',
+          content: () => metaData.value.url,
+        },
+        {
+          property: 'og:title',
+          content: () => metaData.value.title,
+        },
+        {
+          property: 'og:image',
+          content: () => product.value.thumbnail,
+        },
+        {
+          property: 'og:description',
+          content: () => metaData.value.description,
+        },
+        {
+          name: 'twitter:title',
+          content: () => metaData.value.title,
+        },
+        {
+          name: 'twitter:url',
+          content: () => metaData.value.url,
+        },
+        {
+          name: 'twitter:image',
+          content: () => product.value.thumbnail,
+        },
+        {
+          name: 'twitter:description',
+          content: () => metaData.value.description,
+        },
+      ],
+    })
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img :src="product.thumbnail" alt="">
+    <p>{{ product.title }}</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
